985ed13416439c0bb1277e81d684e4e6afac4b90
Павел <genetikq@yandex.ru>
2026-06-29 03:15:01 +0500

    Add player movement with new Input System

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
