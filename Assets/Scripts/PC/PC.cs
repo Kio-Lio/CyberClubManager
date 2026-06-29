94492fef978619a97a1b6eef2cf693a87ddd2fb0
Павел <genetikq@yandex.ru>
2026-06-29 16:41:40 +0500

    Add PC states

--- Assets/Scripts/PC/PC.cs
+++ Assets/Scripts/PC/PC.cs
@@ -1,9 +1,39 @@
 using UnityEngine;
 
+public enum PCState
+{
+    Free,
+    Occupied,
+    Broken
+}
+
 public class PC : MonoBehaviour, IInteractable
 {
+    [SerializeField] private PCState state = PCState.Free;
+
+    public PCState State => state;
+
+    public void SetState(PCState newState)
+    {
+        state = newState;
+    }
+
     public void Interact()
     {
-        Debug.Log("Игрок взаимодействует с ПК");
+        switch (state)
+        {
+            case PCState.Free:
+                Debug.Log("ПК свободен. Можно посадить клиента.");
+                break;
+            case PCState.Occupied:
+                Debug.Log("ПК занят");
+                break;
+            case PCState.Broken:
+                Debug.Log("ПК сломан, требуется ремонт");
+                break;
+            default:
+                Debug.LogWarning($"Неизвестное состояние ПК: {state}");
+                break;
+        }
     }
 }
